204cf1f4b86674a421dc4bf2602a63801de96610
Musafeer-xyz <tanjimhassan666@gmail.com>
2026-09-26 10:06:58 +0600

    server ping

--- server/src/server.js
+++ server/src/server.js
@@ -78,14 +78,17 @@ app.use((err, req, res, next) => {
   })
 })
 
+//cron-active
+app.get('/ping', (req, res) => {
+    res.status(200).send('Server is active');
+});
+
+
 // 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' })
 })
 
-app.get('/ping', (req, res) => {
-    res.status(200).send('Server is active');
-});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
