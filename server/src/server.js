d9f4d0cf92d0ff10b77162580ebc2c1e2b079771
Musafeer-xyz <tanjimhassan666@gmail.com>
2026-09-26 09:56:21 +0600

    server active

--- server/src/server.js
+++ server/src/server.js
@@ -83,6 +83,10 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' })
 })
 
+app.get('/ping', (req, res) => {
+    res.status(200).send('Server is active');
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
